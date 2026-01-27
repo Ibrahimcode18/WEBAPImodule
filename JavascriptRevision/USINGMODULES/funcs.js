90be0b10b67980b15e4780844cf73df979643826
Ibrahim Fatade <ibfatade18@gmail.com>
2026-01-27 13:08:13 +0000

    Use of modules

---
+++ JavascriptRevision/USINGMODULES/funcs.js
@@ -0,0 +1,17 @@
+const func1 = (number1, number2) => {
+    return number1 * number2;
+}
+
+const func2 = (number1, number2) => {
+    return number1 + number2;
+}
+
+const func3 = (number1, number2) => {
+    return true;
+}
+
+module.exports = {
+    func1,
+    func2,
+    func3
+};
