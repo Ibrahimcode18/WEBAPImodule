b2210c85ac2be40a040f20defdd9b26fc6f018c8
Ibrahim Fatade <ibfatade18@gmail.com>
2026-01-28 12:49:13 +0000

    Correct error in promise implementation

--- JavascriptRevision/Callbacks/promise.js
+++ JavascriptRevision/Callbacks/promise.js
@@ -1,12 +1,10 @@
 // file: fetchExample.js
 const fyy = require("fs").promises;
-async function getData() {
- try {
- const data = await fyy.readFile("example.txt", "utf8");
- console.log("Post data:", data);
- } catch (error) {
- console.error("Error fetching data:", error);
- }
-}
-getData();
-console.log("Fetching data in the background...");
+fyy.readFile("example.txt", "utf8")
+    .then(data => {
+        console.log("Who dey breath: ", data);
+    }).catch(err => {
+        console.log("Error: ", error)
+    })
+console.log("Picke me first");
+
