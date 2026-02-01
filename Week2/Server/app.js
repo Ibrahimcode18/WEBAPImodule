170c10a833f3fed378040e7173392a51b9fbd5e4
Ibrahim Fatade <ibfatade18@gmail.com>
2026-02-01 16:16:06 +0000

    Project cleanup

--- Week2/Server/app.js
+++ Week2/Server/app.js
@@ -1,19 +1,10 @@
+// app.js
 const Koa = require('koa');
-const cors = require('@koa/cors'); // Import cors
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 const app = new Koa();
-const router = new Router();
-app.use(cors()); // Enable CORS for everyone
-app.use(bodyParser());
-let articles = [
-    { title: 'Hello World', fullText: 'This is my first article from Koa!' },
-    { title: 'Vue is cool', fullText: 'We will learn Vue next week.' },
-    { title: 'CORS Fixed', fullText: 'Now the browser allows the connection.' }
-];
-router.get('/api/v1/articles', (ctx) => {
-    ctx.body = articles;
-});
-app.use(router.routes());
+const articles = require('./routes/articles.js');
+
+app.use(cors());
+app.use(articles.routes());
 app.listen(3000);
 console.log('Server running on port 3000');
