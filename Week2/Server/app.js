2247b0c6263174a2e0b118c9eb427c83c1b25d71
Ibrahim Fatade <ibfatade18@gmail.com>
2026-02-01 07:11:58 +0000

    Rebuild server with CORS

--- Week2/Server/app.js
+++ Week2/Server/app.js
@@ -1,15 +1,16 @@
 const Koa = require('koa');
+const cors = require('@koa/cors'); // Import cors
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const router = new Router();
+app.use(cors()); // Enable CORS for everyone
 app.use(bodyParser());
-// Mock Data (Since we don't have a database yet)
 let articles = [
 { title: 'Hello World', fullText: 'This is my first article from Koa!' },
-{ title: 'Vue is cool', fullText: 'We will learn Vue next week.' }
+{ title: 'Vue is cool', fullText: 'We will learn Vue next week.' },
+{ title: 'CORS Fixed', fullText: 'Now the browser allows the connection.' }
 ];
-// Define a Route
 router.get('/api/v1/articles', (ctx) => {
 ctx.body = articles;
 });
