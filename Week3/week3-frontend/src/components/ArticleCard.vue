0160f3e94295683a9c8bc34c38fd9822b95fd558
Ibrahim Fatade <ibfatade18@gmail.com>
2026-02-08 14:55:34 +0000

    Create a new component: Article Card

---
+++ Week3/week3-frontend/src/components/ArticleCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+ // Logic will go here
+</script>
+
+<template>
+    <div class="article-card">
+        <h2>Article Title Placeholder</h2>
+        <p>Article Text Placeholder</p>
+    </div>
+</template>
+
+<style scoped>
+ /* Move the CSS from App.vue to here */
+    .article-card {
+        border: 1px solid #ccc;
+        padding: 10px;
+        margin: 10px 0;
+        border-radius: 8px;
+        background-color: #f9f9f9;
+        }
+</style>
